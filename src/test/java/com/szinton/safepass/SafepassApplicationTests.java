db52499b5f2916fcee6cdda5f35ed4929760849c
Szymon Szafranski <szinton@gmail.com>
2022-01-15 10:18:40 +0100

    Initial commit

---
+++ src/test/java/com/szinton/safepass/SafepassApplicationTests.java
@@ -0,0 +1,13 @@
+package com.szinton.safepass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SafepassApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
